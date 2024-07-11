ef30a5dcc405c5d227949348a98b5bcebdf342af
Ayman mahmoud <141870242+Aymanmahmoud79@users.noreply.github.com>
2024-07-11 17:51:53 +0300

    Update 00.JPG.ino

--- 00.JPG.ino
+++ 00.JPG.ino
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 #include <esp32cam.h>
  
-const char* WIFI_SSID = "Hello";
-const char* WIFI_PASS = "Hello1234";
+const char* WIFI_SSID = "***********";
+const char* WIFI_PASS = "***********";
  
 WebServer server(80);
  
